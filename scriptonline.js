2446ec8b60f601e8973cc9332a995aacf28291ad
Naveengnkcoding <89003953+Naveengnkcoding@users.noreply.github.com>
2022-01-31 11:14:30 +0530

    Update scriptonline.js

--- scriptonline.js
+++ scriptonline.js
@@ -8,9 +8,11 @@ var designi =
 {
 	bk : "file:///android_res/drawable/back.jpg",
 	///for android: file:///android_res/drawable/back.jpg
-	frn : "file:///android_res/drawable/skydron.png"
+        frn : "https://rawcdn.githack.com/Naveengnkcoding/helloworld/main/happy.jpg"
+	//frn : "file:///android_res/drawable/skydron.png"
 	//for android: file:///android_res/drawable/skydron.png
-}
+        //For jpg happy: https://rawcdn.githack.com/Naveengnkcoding/helloworld/main/happy.jpg
+}       
 var designd =
 {
 	bk : "file:///android_res/drawable/back2.jpg",
